f5e21e706bea3edb112ebcfdbfd4d4dd35082137
Alex Rønne Petersen <alex@alexrp.com>
2026-08-03 12:46:11 +0200

    llvm-libc: update to LLVM 23

--- lib/libcxx/libc/hdr/types/size_t.h
+++ lib/libcxx/libc/hdr/types/size_t.h
@@ -1,22 +1,23 @@
-//===-- Definition of macros from limits.h --------------------------------===//
+//===-- Proxy for size_t --------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-
-#ifndef LLVM_LIBC_HDR_LIMITS_MACROS_H
-#define LLVM_LIBC_HDR_LIMITS_MACROS_H
+#ifndef LLVM_LIBC_HDR_TYPES_SIZE_T_H
+#define LLVM_LIBC_HDR_TYPES_SIZE_T_H
 
 #ifdef LIBC_FULL_BUILD
 
-#include "include/llvm-libc-macros/limits-macros.h"
+#include "include/llvm-libc-types/size_t.h"
 
-#else // Overlay mode
+#else
 
-#include <limits.h>
+#define __need_size_t
+#include <stddef.h>
+#undef __need_size_t
 
 #endif // LIBC_FULL_BUILD
 
-#endif // LLVM_LIBC_HDR_LIMITS_MACROS_H
+#endif // LLVM_LIBC_HDR_TYPES_SIZE_T_H
--- lib/libcxx/libc/hdr/types/wint_t.h
+++ lib/libcxx/libc/hdr/types/wint_t.h
@@ -1,22 +1,23 @@
-//===-- Definition of macros from limits.h --------------------------------===//
+//===-- Definition of wint_t.h --------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_LIBC_HDR_LIMITS_MACROS_H
-#define LLVM_LIBC_HDR_LIMITS_MACROS_H
+#ifndef LLVM_LIBC_HDR_TYPES_WINT_T_H
+#define LLVM_LIBC_HDR_TYPES_WINT_T_H
 
 #ifdef LIBC_FULL_BUILD
 
-#include "include/llvm-libc-macros/limits-macros.h"
+#include "include/llvm-libc-types/wint_t.h"
 
-#else // Overlay mode
+#else // overlay mode
 
-#include <limits.h>
+#include "hdr/wchar_overlay.h"
 
 #endif // LIBC_FULL_BUILD
 
-#endif // LLVM_LIBC_HDR_LIMITS_MACROS_H
+#endif // LLVM_LIBC_HDR_TYPES_WINT_T_H
