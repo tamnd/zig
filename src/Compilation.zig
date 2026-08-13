c0363560f1fe9591e7b9ba5ccd4bf4b7bd520952
Alex Rønne Petersen <alex@alexrp.com>
2026-08-13 05:14:35 +0200

    Compilation: remove __serenity__ macro
    https://github.com/llvm/llvm-project/pull/187941

--- src/Compilation.zig
+++ src/Compilation.zig
@@ -6291,8 +6291,6 @@ fn addCommonCCArgs(
                 // LLVM doesn't distinguish between Solaris and illumos, but the illumos GCC fork
                 // defines this macro.
                 .illumos => try argv.append("__illumos__"),
-                // This macro has not yet been upstreamed by SerenityOS to Clang.
-                .serenity => try argv.append("__serenity__"),
                 // Homebrew targets without LLVM support; use communities's preferred macros.
                 .@"3ds" => try argv.append("-D__3DS__"),
                 .wiiu => try argv.append("-D__WIIU__"),
